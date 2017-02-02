bde1580fbb0214565d28b8ab3986428520df254d
dclelland0781 <david@getoperatr.com>
2017-02-01 23:27:50 -0500

    classwork commit

--- class16/form-test/app.js
+++ class16/form-test/app.js
@@ -6,6 +6,15 @@ $(document).ready(function () {
 		$('#target').html(name);
 		event.preventDefault();
 	});
-
-
 });
+
+
+// $(document).ready(function () {
+// 	var name;
+
+// 	$('#submit').click(function(event){
+// 		name = $('#name-input').val();
+// 		$('#target').html(name);
+// 		event.preventDefault();
+// 	});
+// });
